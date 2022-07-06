9774e78a7b514c265c314cf33490783415f2d6c4
Aaron Kim <aaron@spoonradio.co>
2022-07-06 14:08:28 +0900

    demo 작업 중

--- app/src/main/java/club/rooftop/demo/MainActivity.kt
+++ app/src/main/java/club/rooftop/demo/MainActivity.kt
@@ -3,6 +3,7 @@ package club.rooftop.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import club.rooftop.demo.ui.theme.RausTheme
+import club.rooftop.raus.components.button.SolidButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +20,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             RausTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column {
+                        SolidButton(text = "Hello world") {
+
+                        }
+                    }
                 }
             }
         }
