bba8ce0cabadd60d59a6f94bcaa297609ca4fa5d
Aaron Kim <aaron@spoonradio.co>
2022-07-06 12:28:52 +0900

    TextStyle define

---
+++ raus/src/main/java/club/rooftop/raus/foundation/typography/TextStyle.kt
@@ -0,0 +1,139 @@
+package club.rooftop.raus.style.typography
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+val PoppinsBold: TextStyle by lazy {
+    TextStyle(
+        fontSize = 26.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+val TitleBold: TextStyle by lazy {
+    TextStyle(
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+val TitleBoldCenter: TextStyle by lazy {
+    TextStyle(
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+}
+
+val SubheadRegular: TextStyle by lazy {
+    TextStyle(fontSize = 16.sp)
+}
+
+val SubheadRegularCenter: TextStyle by lazy {
+    TextStyle(
+        fontSize = 16.sp,
+        textAlign = TextAlign.Center
+    )
+}
+
+val SubheadBold: TextStyle by lazy {
+    TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+    )
+}
+
+val SubheadBoldCenter: TextStyle by lazy {
+    TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+}
+
+val Body1Regular: TextStyle by lazy {
+    TextStyle(fontSize = 14.sp)
+}
+
+val Body1RegularCenter: TextStyle by lazy {
+    TextStyle(
+        fontSize = 14.sp,
+        textAlign = TextAlign.Center
+    )
+}
+
+val Body1RegularEnd: TextStyle by lazy {
+    TextStyle(
+        fontSize = 14.sp,
+        textAlign = TextAlign.End
+    )
+}
+
+val Body1Bold: TextStyle by lazy {
+    TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+val Body1BoldCenter: TextStyle by lazy {
+    TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+}
+
+val Body2Regular: TextStyle by lazy {
+    TextStyle(fontSize = 12.sp)
+}
+
+val Body2RegularCenter: TextStyle by lazy {
+    TextStyle(
+        fontSize = 12.sp,
+        textAlign = TextAlign.Center
+    )
+}
+
+val Body2Bold: TextStyle by lazy {
+    TextStyle(
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+    )
+}
+
+val Body2BoldCenter: TextStyle by lazy {
+    TextStyle(
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+}
+
+val Body3Regular: TextStyle by lazy {
+    TextStyle(fontSize = 10.sp)
+}
+
+val Headline2Bold: TextStyle by lazy {
+    TextStyle(
+        fontSize = 28.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+val Headline3Bold: TextStyle by lazy {
+    TextStyle(
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+val Headline3BoldCenter: TextStyle by lazy {
+    TextStyle(
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+}
