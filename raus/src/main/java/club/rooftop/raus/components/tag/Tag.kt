11ef6d13e869623767df7624b6e36f6ac492db57
Aaron <96655366+youknow-yoonho@users.noreply.github.com>
2022-07-29 09:05:24 +0900

    HBB-909 [Design 개선] Tag Component (#2)
    * Tag 샘플 기본 구현
    
    * Tag 구현

---
+++ raus/src/main/java/club/rooftop/raus/components/tag/Tag.kt
@@ -0,0 +1,70 @@
+package club.rooftop.raus.components.tag
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import club.rooftop.raus.foundation.color.Black
+import club.rooftop.raus.foundation.color.Gray10
+import club.rooftop.raus.foundation.typography.Body1Regular
+import club.rooftop.raus.foundation.typography.SubheadRegular
+
+@Composable
+fun Tag(
+    modifier: Modifier = Modifier,
+    size: TagSize = TagSize.L,
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.height(size.height),
+        elevation = ButtonDefaults.elevation(0.dp, 0.dp, 0.dp, 0.dp, 0.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Gray10,
+            contentColor = Black,
+        ),
+        contentPadding = PaddingValues(horizontal = size.horizontalInnerPadding),
+    ) {
+        Text(
+            text = text,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            style = size.textStyle
+        )
+    }
+}
+
+enum class TagSize(
+    val horizontalInnerPadding: Dp,
+    val height: Dp,
+    val textStyle: TextStyle
+) {
+    L(12.dp, 40.dp, SubheadRegular),
+    M(12.dp, 34.dp, Body1Regular),
+    S(8.dp, 26.dp, Body1Regular),
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSolidTag() {
+    Tag(text = "Keyword", size = TagSize.L) {
+
+    }
+    Tag(text = "Keyword", size = TagSize.M) {
+
+    }
+    Tag(text = "Keyword", size = TagSize.S) {
+
+    }
+}
